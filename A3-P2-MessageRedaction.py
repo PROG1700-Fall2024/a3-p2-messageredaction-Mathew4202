b9bca0ac63812d849fdb11ec9fa5095f3f1ddb0a
Mathew4202 <w0461513@campus.nscc.ca>
2024-11-17 02:44:34 -0400

    Update A3-P2-MessageRedaction.py

--- A3-P2-MessageRedaction.py
+++ A3-P2-MessageRedaction.py
@@ -13,10 +13,10 @@ def main():
     while (sentencePhrase != "quit"):
         sentencePhrase = input("Type a phrase (or quit to exit program): ").lower()
         if sentencePhrase == "quit":
-            break
-        phrase.append(sentencePhrase)
-        redact = input("Type a comma-seperated list of letters to redact: ").replace(" ", "").split(',')
-        redactedPhrase = ['_' if char in redact else char for char in sentencePhrase]
+            break #if user types quit it ends the loop
+        phrase.append(sentencePhrase) #This appends what the user inputs into a list
+        redact = input("Type a comma-seperated list of letters to redact: ").replace(" ", "").split(',') # whatever the user inputs it replaces it and splits it equally
+        redactedPhrase = ['_' if char in redact else char for char in sentencePhrase] # this just means that replace the charcters the user enters in redact with _ else leave the other characters as it is in the input sentencePhrace
         redactedCount = sum(sentencePhrase.count(letter) for letter in redact)
         print("Number of letters redacted: ",redactedCount)
         print("Redacted Phrase: ", ''.join(redactedPhrase))
